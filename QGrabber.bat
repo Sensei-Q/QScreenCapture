b8b883836b0c04addbc8ccad2edac4e8c850d552
Sensei-Q <97165905+Sensei-Q@users.noreply.github.com>
2022-01-09 11:36:26 +0100

    Update QGrabber.bat
    QROOT and PICTURES can now be set to the current directory (leave them empty).

--- QGrabber.bat
+++ QGrabber.bat
@@ -1,22 +1,22 @@
-:: QGrabber.bat v1.1 (c) 2022 Sensei (aka 'Q')
+:: QGrabber.bat v1.2 (c) 2022 Sensei (aka 'Q')
 :: A batch script for Windows that waits a specified number of seconds and takes a screenshot in a loop.
 ::
 @echo off
-SET QROOT="[edit-me]"
-SET PICTURES="C:\Users\[user-name-edit-me]\Pictures"
+SET QROOT="[edit-me]\"
+SET PICTURES="C:\Users\[user-name-edit-me]\Pictures\Screen-shots\"
 SET OPTIONS=-v -d
 
 :: Delay in seconds.
 SET DELAY=1
 
 :: Set to 0 to have an infinite loop.
-:: Set to >0 to have a finite loop.
+:: Set to >0 to have an finite loop.
 SET COUNT=0
 
 ECHO Screen grabbing to the %PICTURES% folder with a delay of %DELAY% second(s)..
 IF %COUNT%==0 ECHO Infinite loop. Use Ctrl-C to break it.
 :loop
-   %QROOT%\QScreenCapture %OPTIONS% %PICTURES%\picture.png
+   "%QROOT%QScreenCapture" %OPTIONS% "%PICTURES%picture.png"
    IF %COUNT%==0 GOTO next
    SET /A COUNT-=1
    IF %COUNT%==0 GOTO end
